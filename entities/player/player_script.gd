a915b8db39782e5b0eb5e7f5eca11baa981b6a5d
Albert Valo <valo@kaworu.local>
2022-02-18 23:30:23 +0100

    player art added

--- entities/player/player_script.gd
+++ entities/player/player_script.gd
@@ -4,20 +4,53 @@ export (int) var speed = 200
 
 var velocity = Vector2.ZERO
 
+enum {
+	IDLE,
+	WALK,
+	HIT
+}
+
+var state = IDLE
+
 func get_input():
 	velocity = Vector2.ZERO
+	if state != HIT:
+		state = IDLE
+		
 	if Input.is_action_pressed('right'):
 		velocity.x += 1
+		state = WALK
 	if Input.is_action_pressed('left'):
 		velocity.x -= 1
+		state = WALK
 	if Input.is_action_pressed('down'):
 		velocity.y += 1
+		state = WALK
 	if Input.is_action_pressed('up'):
 		velocity.y -= 1
+		state = WALK
+	if Input.is_action_just_pressed("attack"):
+		state = HIT
+		$AttackTimer.start()
 	
 	velocity = velocity.normalized() * speed
 
 func _physics_process(_delta):
+	
+			
 	look_at(get_global_mouse_position())
 	get_input()
+	match state:
+		IDLE:
+			$AnimationPlayer.play("idle")
+		WALK:
+			$AnimationPlayer.play("walk")
+		HIT:
+			$AnimationPlayer.play("hit")
+			
 	velocity = move_and_slide(velocity)
+
+
+func _on_AttackTimer_timeout():
+	state = IDLE
+	pass # Replace with function body.
